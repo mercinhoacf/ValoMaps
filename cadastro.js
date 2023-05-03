3360940188bcf2aa16b325f74e845b786d8bfcea
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-05-03 11:23:50 -0300

    Funcionalidade tela de cadastro.

---
+++ cadastro.js
@@ -0,0 +1,68 @@
+body {
+  background-color: #1C1C1C;
+}
+
+label {
+  color: #ff4655;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+input {
+  background-color: #ff4655;
+  height: 25px;
+  border-radius: 7px;
+}
+input::placeholder {
+  color: #1C1C1C;
+  font-size: 15px;
+  text-align: left;
+}
+
+button {
+  background-color: #ff4655;
+  padding: 10px 20px ;
+  border-radius: 7px;
+}
+
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #ff4655;
+  font-weight: 900
+}
+
+main {
+  margin: auto;
+  display: flex;
+  flex-flow: column;
+  width: 20%;
+  gap: 0px;
+}
+
+.cabecalho{
+  background-color: #ff4655;
+  margin: auto;
+  display: flex;
+  width: 100%;
+  height:40px;
+  align-items: center;
+}
+
+p{
+  color: #fff;
+  font-size: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 900;
+  padding-left: 10px;
+  text-shadow: 2px 2px 2px  #1C1C1C;
+}
+
+.gekko{
+  width: 50px;
+  height: 30px;
+}
+
+.jett{
+  width: 50px;
+  height: 30px;
+}
